4d617fc26dc773da75140930b3f119c506ad227c
Adam Drong <adamdrong@Macbook-Pro-Aidro.local>
2024-10-09 11:01:42 +0200

    change

--- Automation/tf/provider.tf
+++ Automation/tf/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
 provider "proxmox" {
  pm_api_url          = var.proxmox_url
- pm_api_token_id     = var.proxmox_apikey
- pm_api_token_secret = var.proxmox_apitoken
+ pm_user             = var.proxmox_user
+ pm_password         = var.proxmox_password
  pm_tls_insecure     = true
 }
