3c3be2dc2843b2f15e0948e215e048b11228004b
Adam Drong <adamdrong@Macbook-Pro-Aidro.local>
2024-10-09 11:00:23 +0200

    change

--- Automation/tf/var.tf
+++ Automation/tf/var.tf
@@ -92,59 +92,9 @@ variable "vm_name" {
     nullable = false
 }
 
-variable "qemu_os" {
-    type = string
-    default = "l26"
-    nullable = false
-}
-
 
 ### LXC variables
 variable "ct_name" {
     type = string
     default = "test" 
-}
-
-variable "ct_bridge" {
-  type = string
-}
-
-variable "ct_datastore_storage_location" {
-  type = string
-}
-
-variable "ct_datastore_template_location" {
-  type = string
-}
-
-variable "ct_nic_rate_limit" {
-  type = number
-}
-
-variable "ct_memory" {
-  type = number
-}
-
-variable "ct_source_file_path" {
-  type = string
-}
-
-variable "dns_domain" {
-  type = string
-}
-
-variable "dns_server" {
-  type = string
-}
-
-variable "gateway" {
-  type = string
-}
-
-variable "os_type" {
-  type = string
-}
-
-variable "tmp_dir" {
-  type = string
 }
