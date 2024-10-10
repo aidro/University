a506fc37ea309d647589e7ae40aeb3f5b1dcdc1f
Adam Drong <adamdrong@client-145-100-224-117.surfnet.eduroam.rug.nl>
2024-10-10 14:38:32 +0200

    changes

--- Automation/tf/lxc.tf
+++ Automation/tf/lxc.tf
@@ -16,34 +16,71 @@ resource "proxmox_lxc" "basic" {
     ip     = var.ip_address
   }
 
-  # Cloud-init script to install WordPress
-  sshkeys = file("~/.ssh/id_rsa.pub")
-  # ipconfig0 = "ip=dhcp"
+  # Use provisioners to install Docker and configure services
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "root"
+      password = "your_password"  # Password-based SSH authentication
+      host     = self.ipv4_address
+    }
 
-  # User data to provision WordPress and required services
-  cloud_init = <<EOF
-    #!/bin/bash
-    apt update -y
-    apt install -y apache2 php libapache2-mod-php mariadb-server php-mysql wget
+    # Step 1: Install Docker
+    inline = [
+      "apt-get update",
+      "apt-get install -y apt-transport-https ca-certificates curl software-properties-common",
+      "curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -",
+      "add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable'",
+      "apt-get update",
+      "apt-get install -y docker-ce",
+      "systemctl enable docker",
+      "systemctl start docker",
+    ]
+  }
 
-    # Set up MySQL for WordPress
-    mysql -u root -e "CREATE DATABASE wordpress;"
-    mysql -u root -e "CREATE USER 'wordpressuser'@'localhost' IDENTIFIED BY 'password';"
-    mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpressuser'@'localhost';"
-    mysql -u root -e "FLUSH PRIVILEGES;"
+  # Step 2: Use Docker to deploy WordPress and MySQL
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "root"
+      password = var.password
+      host     = self.ipv4_address
+    }
 
-    # Download and configure WordPress
-    cd /var/www/html
-    wget https://wordpress.org/latest.tar.gz
-    tar -xvzf latest.tar.gz
-    mv wordpress/* .
-    rm -rf wordpress latest.tar.gz
-    chown -R www-data:www-data /var/www/html/
-    chmod -R 755 /var/www/html/
+    inline = [
+      # Install Docker Compose (optional, for easier container management)
+      "curl -L https://github.com/docker/compose/releases/download/v2.29.7/docker-compose-linux-x86_64 /usr/local/bin/docker-compose",
+      "chmod +x /usr/local/bin/docker-compose",
 
-    systemctl restart apache2
-EOF
+      # Create a directory for the WordPress setup
+      "mkdir -p /opt/wordpress",
 
-  autostart = true
+      # Create a Docker Compose file for WordPress and MySQL
+      "echo 'version: \"3\"' > /opt/wordpress/docker-compose.yml",
+      "echo 'services:' >> /opt/wordpress/docker-compose.yml",
+      "echo '  db:' >> /opt/wordpress/docker-compose.yml",
+      "echo '    image: mysql:5.7' >> /opt/wordpress/docker-compose.yml",
+      "echo '    restart: always' >> /opt/wordpress/docker-compose.yml",
+      "echo '    environment:' >> /opt/wordpress/docker-compose.yml",
+      "echo '      MYSQL_ROOT_PASSWORD: somewordpress' >> /opt/wordpress/docker-compose.yml",
+      "echo '      MYSQL_DATABASE: wordpress' >> /opt/wordpress/docker-compose.yml",
+      "echo '      MYSQL_USER: wordpress' >> /opt/wordpress/docker-compose.yml",
+      "echo '      MYSQL_PASSWORD: wordpress' >> /opt/wordpress/docker-compose.yml",
+      "echo '  wordpress:' >> /opt/wordpress/docker-compose.yml",
+      "echo '    depends_on:' >> /opt/wordpress/docker-compose.yml",
+      "echo '      - db' >> /opt/wordpress/docker-compose.yml",
+      "echo '    image: wordpress:latest' >> /opt/wordpress/docker-compose.yml",
+      "echo '    restart: always' >> /opt/wordpress/docker-compose.yml",
+      "echo '    ports:' >> /opt/wordpress/docker-compose.yml",
+      "echo '      - \"8080:80\"' >> /opt/wordpress/docker-compose.yml",
+      "echo '    environment:' >> /opt/wordpress/docker-compose.yml",
+      "echo '      WORDPRESS_DB_HOST: db:3306' >> /opt/wordpress/docker-compose.yml",
+      "echo '      WORDPRESS_DB_USER: wordpress' >> /opt/wordpress/docker-compose.yml",
+      "echo '      WORDPRESS_DB_PASSWORD: wordpress' >> /opt/wordpress/docker-compose.yml",
+      "echo '      WORDPRESS_DB_NAME: wordpress' >> /opt/wordpress/docker-compose.yml",
+
+      # Start the Docker containers
+      "docker-compose -f /opt/wordpress/docker-compose.yml up -d",
+    ]
+  }
 }
- 
