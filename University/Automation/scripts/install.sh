e654353cec64d94b43ee6362c58519ce72499ff1
aidro <adam@drong.net>
2024-10-18 01:07:29 +0200

    change

--- University/Automation/scripts/install.sh
+++ University/Automation/scripts/install.sh
@@ -5,6 +5,7 @@ function install_docker() {
     if ! command -v docker 2>&1 >/dev/null
     then
         apt-get install docker -y && apt-get install docker-compose -y
+        cd /home/wp
         docker-compose up -d --build
     fi
 }
