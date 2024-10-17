a167856702f7ad415df48e397bec183a6b45377b
aidro <adam@drong.net>
2024-10-17 16:58:28 +0200

    change

--- University/Automation/tf/lxc.tf
+++ University/Automation/tf/lxc.tf
@@ -27,16 +27,16 @@ resource "proxmox_lxc" "basic" {
 
   # Use provisioners to install Docker and configure services
 
-  provisioner "file" {
-    connection {
-      type     = "ssh"
-      user     = "root"
-      private_key = file("~/.ssh/id_rsa")
-      host     = "10.24.49.200"
-    }
-    source      = "/opt/University/University/Automation/scripts/test.sh"
-    destination = "/tmp/test.sh"
-  }
+#  provisioner "file" {
+#    connection {
+#      type     = "ssh"
+#      user     = "root"
+#      private_key = file("~/.ssh/id_rsa")
+#      host     = "10.24.49.200"
+#    }
+#   source      = "/opt/University/University/Automation/scripts/test.sh"
+#    destination = "/tmp/test.sh"
+#  }
 
   provisioner "remote-exec" {
     connection {
@@ -45,6 +45,6 @@ resource "proxmox_lxc" "basic" {
       private_key = file("~/.ssh/id_rsa")
       host     = "10.24.49.200"
     }
-    script = "/tmp/test.sh"
+    script = "/opt/University/University/Automation/scripts/test.sh"
   }
 }
