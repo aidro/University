77027a9749ada5310f72eac562936912ba08a829
aidro <adam@drong.net>
2024-10-17 17:44:01 +0200

    added wordpress config in docker-compose. Trying to see if provisioners can run as sudo.

--- University/Automation/scripts/install.sh
+++ University/Automation/scripts/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+sudo apt-get install docker -y && sudo apt-get install docker-compose -y
 # Install docker
-function docker() {
-    # Install docker on host machine
-    if ! command -v docker 2>&1 >/dev/null
-    then
-        sudo apt-get install docker -y && sudo apt-get install docker-compose -y
-    fi
-}
+# function docker() {
+#     # Install docker on host machine
+#     if ! command -v docker 2>&1 >/dev/null
+#     then
+#         sudo apt-get install docker -y && sudo apt-get install docker-compose -y
+#     fi
+# }
 
-docker
+# docker
